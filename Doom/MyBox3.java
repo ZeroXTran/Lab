525ba05c0fabc5b41098036a99503f4be0e58edf
ZeroXTran <98497908+ZeroXTran@users.noreply.github.com>
2022-01-26 17:41:40 -0600

    Add files via upload

---
+++ Doom/MyBox3.java
@@ -0,0 +1,10 @@
+
+public class MyBox3 {
+    public static void main(String[] args) {
+        System.out.println("*********");
+        System.out.printf ("*\t%c\n", '*');
+        System.out.printf ("*\t%c\n", '*');
+        System.out.printf ("*\t%c\n", '*');
+        System.out.println("*********");
+    }
+}
---
+++ Doom/MyTriangle.java
@@ -0,0 +1,13 @@
+public class MyTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.printf ("*%c\n", '*');
+        System.out.printf ("*%2c\n", '*');
+        System.out.printf ("*%3c\n", '*');
+        System.out.printf ("*%4c\n", '*');
+        System.out.printf ("*%5c\n", '*');
+        System.out.printf ("*%6c\n", '*');
+        System.out.printf ("*%7c\n", '*');
+        System.out.println("*********");
+    }
+}
