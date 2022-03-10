e6eef4bcc26a9648490432227d31caf367df87a2
ZeroXTran <98497908+ZeroXTran@users.noreply.github.com>
2022-03-09 18:17:36 -0600

    Update BoxTest.java

--- Doom/Doom2/BoxTest.java
+++ Doom/Doom2/BoxTest.java
@@ -1,3 +1,10 @@
+/*
+ Dan Tran
+ ITSE-1302-008
+ 3/2/2022
+ PP 4.6
+ Write a class called Box that contains instance data that represents the height, width, and depth of the box.
+*/
 
 public class BoxTest {
 
@@ -6,11 +13,11 @@ public class BoxTest {
 		Box box2 = new Box(3.06,0.08,1.54);
 		Box box3 = new Box(0.05,2.06,2.09);
 		
-		box3.setWidth(2.55);
+	    box3.setWidth(2.55);
 	    box2.setDepth(3.54);
 	    box1.setHeight(2.66);
 		
-		System.out.println(box1);
+	    System.out.println(box1);
 	    System.out.println(box2);
 	    System.out.println(box3);
 	}
