d41b25abb0b98667e2699ba7f01228d930283407
ZeroXTran <98497908+ZeroXTran@users.noreply.github.com>
2022-03-09 18:44:10 -0600

    Update CounterDriver.java

--- Doom/Doom2/CounterDriver.java
+++ Doom/Doom2/CounterDriver.java
@@ -1,4 +1,10 @@
-
+/*
+ Dan Tran
+ ITSE-1302-008
+ 3/2/2022
+ PP 4.1
+ Write a class called Counter that represents a simple tally counter, which might be used to count people as they enter a room.
+*/
 public class CounterDriver {
 	public static void main(String[] args) {
 		Counter obj = new Counter();
