c67dda7b3a1d40aedec86d842b965694efa574d9
ZeroXTran <98497908+ZeroXTran@users.noreply.github.com>
2022-04-29 18:11:24 -0500

    Add files via upload

---
+++ Doom/Doom2/Chapter6/Diamond.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Diamond {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("What char do you want to print?");
+		String str = scan.next();
+		int height = 0;
+		while (true) {
+			System.out.println("How many lines tall should the diamond shape be 3-100?");
+			height = scan.nextInt();
+			if (height >= 3 && height <= 100)
+				break;
+			System.out.println("Wrong height try again ");
+		}
+		String line = str;
+		String space = "";
+		for (int a = 0; a < height / 2 -1; a++)
+			space = "  " + space;
+
+		System.out.println(space + line);
+		for (int i = 0; i < height / 2 ; i++) {
+			if (space.length() > 0)
+				space = space.substring(1);
+			line = str + line + str;
+			System.out.println(space + line);
+
+		}
+		if(height%2==0)
+			space =" ";
+		else
+			space = "";
+		for (int i = 0; i < height / 2; i++) {
+			line =line.substring(2);
+			space = space+" ";
+			System.out.println(space+ line);
+		}
+		
+	}
+
+}
