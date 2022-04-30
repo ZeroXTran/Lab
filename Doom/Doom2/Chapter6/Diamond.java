8748260cc10baf57cc06baa642f8db1021e570a6
ZeroXTran <98497908+ZeroXTran@users.noreply.github.com>
2022-04-30 13:47:41 -0500

    Update Diamond.java

--- Doom/Doom2/Chapter6/Diamond.java
+++ Doom/Doom2/Chapter6/Diamond.java
@@ -1,3 +1,11 @@
+/*
+Dan Tran
+ITSE-1032-008
+4/30/2022
+PP 6.7
+ Create modified versions of the Stars program to print the following patterns. Create a separate program to produce each pattern.
+*/
+
 import java.util.Scanner;
 
 public class Diamond {
@@ -13,11 +21,11 @@ public class Diamond {
 			height = scan.nextInt();
 			if (height >= 3 && height <= 100)
 				break;
-			System.out.println("Wrong height try again ");
+			System.out.println("Wrong height try again! ");
 		}
 		String line = str;
 		String space = "";
-		for (int a = 0; a < height / 2 -1; a++)
+		for (int a = 0; a < height / 2+1; a++)
 			space = "  " + space;
 
 		System.out.println(space + line);
@@ -28,14 +36,14 @@ public class Diamond {
 			System.out.println(space + line);
 
 		}
-		if(height%2==0)
-			space =" ";
-		else
-			space = "";
+		//if(height%2==0)
+			//space =" ";
+		//else
+			//space = "";
 		for (int i = 0; i < height / 2; i++) {
-			line =line.substring(2);
+			line = line.substring(2);
 			space = space+" ";
-			System.out.println(space+ line);
+			System.out.println(space + line);
 		}
 		
 	}
